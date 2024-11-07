10f4b6aa89af73018c5aeff7ce3a22e61de76165
Samuel Aberra <122904898+samkiyya@users.noreply.github.com>
2024-11-07 10:31:11 -0800

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,134 +1,100 @@
 #!/usr/bin/env python3
+"""A module for filtering logs.
 """
-Module for handling and securely logging Personal Data from a database.
-"""
-
-from typing import List
+import os
 import re
 import logging
-import os
 import mysql.connector
-from mysql.connector import MySQLConnection
+from typing import List
 
-# Define the fields that contain PII
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
     """
-    Returns an obfuscated log message where specified fields are replaced by the redaction string.
-    
-    Args:
-        fields (List[str]): List of strings representing PII fields to obfuscate.
-        redaction (str): The string to replace PII field values with.
-        message (str): The original log message that may contain PII.
-        separator (str): The delimiter used to separate fields in the message.
-    
-    Returns:
-        str: The obfuscated log message.
-    """
-    for f in fields:
-        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
-    return message
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
 
 
 def get_logger() -> logging.Logger:
-    """
-    Sets up and returns a Logger object configured to use a RedactingFormatter for PII fields.
-    
-    Returns:
-        logging.Logger: Configured logger instance for logging user data.
+    """Creates a new logger for user data.
     """
     logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
     logger.addHandler(stream_handler)
-
     return logger
 
 
-def get_db() -> MySQLConnection:
-    """
-    Establishes and returns a connector to a MySQL database using credentials from environment variables.
-    
-    Returns:
-        MySQLConnection: A MySQLConnection object for interacting with the MySQL database.
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database.
     """
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-
-    return mysql.connector.connect(
+    connection = mysql.connector.connect(
         host=db_host,
+        port=3306,
         user=db_user,
         password=db_pwd,
         database=db_name,
     )
+    return connection
 
 
 def main():
+    """Logs the information about user records in a table.
     """
-    Retrieves and logs rows from the 'users' table in a MySQL database, filtering sensitive PII fields.
-    
-    This function connects to a MySQL database using get_db(), then queries all rows from the
-    'users' table. Each row is formatted and logged with PII fields obfuscated.
-    """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    field_names = [i[0] for i in cursor.description]
-
-    logger = get_logger()
-
-    for row in cursor:
-        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
-        logger.info(str_row.strip())
-
-    cursor.close()
-    db.close()
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class for obfuscating sensitive fields in log messages.
-    
-    Attributes:
-        REDACTION (str): The string to replace sensitive field values with.
-        FORMAT (str): The log message format.
-        SEPARATOR (str): The delimiter for separating log message fields.
+    """ Redacting Formatter class
     """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """
-        Initializes a RedactingFormatter instance.
-        
-        Args:
-            fields (List[str]): List of strings representing PII fields to obfuscate.
-        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecord.
         """
-        Obfuscates sensitive fields in the log record message before formatting.
-        
-        Args:
-            record (logging.LogRecord): The log record containing the message to format.
-        
-        Returns:
-            str: The formatted and obfuscated log message.
-        """
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
